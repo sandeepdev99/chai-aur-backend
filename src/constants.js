205df03bae6ebe8a4879d36e8d90ed86733b2db3
sandeepdev99 <sandeep99.dev@gmail.com>
2025-12-16 09:51:45 +0530

    set up databse connection

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME = "personal_db";
+
---
+++ src/index.js
@@ -0,0 +1,35 @@
+// require('dotenv').config({path: './env'});
+// //this also works but in causs inconsistency.
+
+import dotenv from "dotenv"
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path : '.env' });
+console.log("PORT:", process.env.PORT);
+const app = express()
+
+// function connectDB{
+//   //code to connect
+// }
+// connectNB();
+/*
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", () => {
+      console.log("ERR:", error);
+      throw error
+    })
+
+    app.listen(process.env.PORT, ()=> {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.log("ERROR",error)
+    throw err
+  }
+})()*/
+
+//importing conection file for reusable and clean index.js
+connectDB()
